a62bdec23a64d9a660df85292d4eed723485a624
tangq <t1339803847@163.com>
2020-02-12 23:22:10 +0800

    '添加md编辑器到文档'

--- front/src/App.vue
+++ front/src/App.vue
@@ -6,30 +6,30 @@
  * @LastEditors: Please set LastEditors
  -->
 <template>
-  <div id="app">
-      <router-view></router-view>
+    <div id="app">
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-export default {
-  name: "app",
-  created(){
-  }
-};
+    export default {
+        name: "app",
+        created() {
+        }
+    };
 </script>
 
 
 <style>
-@import "static/css/base.css";
+    @import "static/css/base.css";
 
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  width: 100%;
-  height: 100%;
-}
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+        width: 100%;
+        height: 100%;
+    }
 
 </style>
--- front/src/main.js
+++ front/src/main.js
@@ -3,7 +3,15 @@ import Vuex from "vuex";
 import App from "./App.vue";
 import router from "./router";
 import VueResource from "vue-resource";
-import "./validate.js";
+import "./validate";
+
+
+//markdown组件
+import mavonEditor from 'mavon-editor';
+import 'mavon-editor/dist/css/index.css';
+
+
+
 
 // Vue.prototype.apiAddress = "http://120.27.241.94:50682";
 Vue.prototype.apiAddress = "http://localhost:1000";
@@ -14,71 +22,60 @@ Vue.config.productionTip = false;
 
 Vue.use(VueResource);
 Vue.use(Vuex);
+Vue.use(mavonEditor);
 
 //扩展指令，设置焦点
 Vue.directive("focus", {
-  inserted: function(el) {
-    el.focus();
-  }
+    inserted: function (el) {
+        el.focus();
+    }
 });
 
 const store = new Vuex.Store({
-  state: {
-    count: 0,
-    userInfo: {},
-    project: {}
-  },
-  mutations: {
-    increment(state) {
-      state.count++;
+    state: {
+        count: 0,
+        userInfo: {},
+        project: {}
     },
-    saveUserInfo(state, user) {
-      state.userInfo = user;
-    },
-    saveProject(state, project) {
-      state.project = project;
+    mutations: {
+        increment(state) {
+            state.count++;
+        },
+        saveUserInfo(state, user) {
+            state.userInfo = user;
+        },
+        saveProject(state, project) {
+            state.project = project;
+        }
     }
-  }
 });
 
-//扩展指令，验证
-Vue.directive("validate", {
-  update: function(el, binding) {
-    el.onblur = function() {
-      let testMatch = /^test$/;
-      if (testMatch.test(binding.value)) {
-        return;
-      }
-    };
-  }
-});
 
 //拦截器拦截请求,添加token
 Vue.http.interceptors.push(request => {
-  if (request.method === "GET") {
-    if (!request.params.token) {
-      request.params.token = store.state.userInfo.token;
+    if (request.method === "GET") {
+        if (!request.params.token) {
+            request.params.token = store.state.userInfo.token;
+        }
+    } else if (request.method === "POST") {
+        if (!request.body.token) {
+            request.body.token = store.state.userInfo.token;
+        }
+        if (request.emulateJSON !== true) {
+            request.emulateJSON = true;
+        }
     }
-  } else if (request.method === "POST") {
-    if (!request.body.token) {
-      request.body.token = store.state.userInfo.token;
-    }
-    if (request.emulateJSON !== true) {
-      request.emulateJSON = true;
-    }
-  }
-  return response => {
-    if (response.body.code && response.body.code == 34) {
-      alert("超时,重新登录");
-      localStorage.removeItem("userInfo");
-      router.push("/login");
-      return;
-    }
-  };
+    return response => {
+        if (response.body.code && response.body.code === 34) {
+            alert("超时,重新登录");
+            localStorage.removeItem("userInfo");
+            router.push("/login");
+        }
+    };
 });
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
