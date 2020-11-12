af8baf4b0b663198be4e34503253c580ca87dd6e
tangzq <t1339803847@163.com>
2020-11-12 21:21:12 +0800

    解决获取用户信息失败的问题

--- front/src/components/user/myCenter.vue
+++ front/src/components/user/myCenter.vue
@@ -8,49 +8,73 @@
         <ul>
           <li>
             <em>登录名：</em>
-            <p>{{userInfo.name}}</p>
+            <p>{{ userInfo.name }}</p>
           </li>
           <li class="nick-name-li">
             <em>昵称：</em>
-            <p>{{userInfo.nick_name}}</p>
+            <p>{{ userInfo.nick_name }}</p>
           </li>
           <li>
             <em>邮箱：</em>
-            <p>{{userInfo.email}}</p>
+            <p>{{ userInfo.email }}</p>
           </li>
           <li>
             <em>创建时间：</em>
-            <p>{{userInfo.create_time}}</p>
+            <p>{{ userInfo.create_time }}</p>
           </li>
           <li>
             <em>用户权限：</em>
-            <p>{{userInfo.type_text}}</p>
+            <p>{{ userInfo.type_text }}</p>
           </li>
           <li>
             <em>用户状态：</em>
-            <p>{{userInfo.state_text}}</p>
+            <p>{{ userInfo.state_text }}</p>
           </li>
           <li>
             <em>手机：</em>
-            <p>{{userInfo.mobile_number ? userInfo.mobile_number : 'unknown'}}</p>
+            <p>
+              {{ userInfo.mobile_number ? userInfo.mobile_number : "unknown" }}
+            </p>
           </li>
         </ul>
         <div class="btn">
-          <el-button @click="dialogFormVisible = true" type="primary" size="small">修改密码</el-button>
-          <el-button size="small"  type="primary" @click="updateNickName()">修改昵称</el-button>
+          <el-button
+            @click="dialogFormVisible = true"
+            type="primary"
+            size="small"
+            >修改密码</el-button
+          >
+          <el-button size="small" type="primary" @click="updateNickName()"
+            >修改昵称</el-button
+          >
         </div>
       </div>
     </div>
     <el-dialog title="新建用户" :visible.sync="dialogFormVisible" width="30%">
       <el-form :model="form" label-width="80px" ref="form" :rules="rules">
         <el-form-item label="旧密码" prop="old_pwd">
-          <el-input v-model="form.old_pwd" autocomplete="off" type="password" placeholder="旧密码"></el-input>
+          <el-input
+            v-model="form.old_pwd"
+            autocomplete="off"
+            type="password"
+            placeholder="旧密码"
+          ></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="pwd">
-          <el-input v-model="form.pwd" autocomplete="off" type="password" placeholder="密码"></el-input>
+          <el-input
+            v-model="form.pwd"
+            autocomplete="off"
+            type="password"
+            placeholder="密码"
+          ></el-input>
         </el-form-item>
         <el-form-item label="重复密码" prop="re_pwd">
-          <el-input v-model="form.re_pwd" autocomplete="off" type="password" placeholder="重复密码"></el-input>
+          <el-input
+            v-model="form.re_pwd"
+            autocomplete="off"
+            type="password"
+            placeholder="重复密码"
+          ></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -94,7 +118,7 @@ export default {
       form: {
         old_pwd: "",
         pwd: "",
-        re_pwd: ""
+        re_pwd: "",
       },
       dialogFormVisible: false,
       rules: {
@@ -104,8 +128,8 @@ export default {
             min: 6,
             max: 50,
             message: "长度在 6 到 50 个字符",
-            trigger: "blur"
-          }
+            trigger: "blur",
+          },
         ],
         pwd: [
           { required: true, message: "请输入密码", trigger: "blur" },
@@ -113,9 +137,9 @@ export default {
             min: 6,
             max: 50,
             message: "长度在 6 到 50 个字符",
-            trigger: "blur"
+            trigger: "blur",
           },
-          { validator: validatePass, trigger: "blur" }
+          { validator: validatePass, trigger: "blur" },
         ],
         re_pwd: [
           { required: true, message: "请再次输入密码", trigger: "blur" },
@@ -123,11 +147,11 @@ export default {
             min: 6,
             max: 50,
             message: "长度在 6 到 50 个字符",
-            trigger: "blur"
+            trigger: "blur",
           },
-          { validator: reValidatePass, trigger: "blur" }
-        ]
-      }
+          { validator: reValidatePass, trigger: "blur" },
+        ],
+      },
     };
   },
   created() {
@@ -139,23 +163,27 @@ export default {
   methods: {
     //获取用户信息
     getUserInfo() {
-      this.$http.get( "/user/get-user-info").then(
-        res => {
-          let response = res.data;
-          if (response.code === CODE_OK) {
-            this.userInfo = response.data;
-          } else {
-            this.$message.error("failed:" + response.msg);
+      this.$http
+        .get("/user/get-user-info", {
+          params: {},
+        })
+        .then(
+          (res) => {
+            let response = res.data;
+            if (response.code === CODE_OK) {
+              this.userInfo = response.data;
+            } else {
+              this.$message.error("failed:" + response.msg);
+            }
+          },
+          () => {
+            this.$message.error("获取数据失败");
           }
-        },
-        () => {
-          this.$message.error("获取数据失败");
-        }
-      );
+        );
     },
     //更新密码
     updatePwd() {
-      this.$refs.form.validate(valid => {
+      this.$refs.form.validate((valid) => {
         if (valid) {
           if (this.form.pwd === this.form.old_pwd) {
             this.$message.error("新旧密码不能相同");
@@ -163,17 +191,13 @@ export default {
           }
 
           this.$http
-            .post(
-               "/user/update-pwd",
-              {
-                oldPwd: this.form.old_pwd,
-                newPwd: this.form.pwd,
-                rePwd: this.form.re_pwd
-              },
-             
-            )
+            .post("/user/update-pwd", {
+              oldPwd: this.form.old_pwd,
+              newPwd: this.form.pwd,
+              rePwd: this.form.re_pwd,
+            })
             .then(
-              res => {
+              (res) => {
                 let response = res.data;
                 if (response.code !== CODE_OK) {
                   this.$message.error(response.msg);
@@ -199,7 +223,7 @@ export default {
         cancelButtonText: "取消",
         inputValue: this.userInfo.nick_name,
         inputPattern: /.{1,50}/,
-        inputErrorMessage: "昵称不正确"
+        inputErrorMessage: "昵称不正确",
       })
         .then(({ value }) => {
           //验证名字是否一样
@@ -209,15 +233,11 @@ export default {
           }
 
           this.$http
-            .post(
-               "/user/update-nickname",
-              {
-                nickname: value
-              },
-             
-            )
+            .post("/user/update-nickname", {
+              nickname: value,
+            })
             .then(
-              res => {
+              (res) => {
                 let response = res.data;
                 if (response.code !== CODE_OK) {
                   this.$message.error("failed:" + response.msg);
@@ -228,7 +248,7 @@ export default {
                   this.$store.commit("saveUserInfo", this.userInfo);
                   this.$message({
                     type: "success",
-                    message: "你的新昵称是: " + value
+                    message: "你的新昵称是: " + value,
                   });
                 }
               },
@@ -240,12 +260,12 @@ export default {
         .catch(() => {
           this.$message({
             type: "info",
-            message: "取消输入"
+            message: "取消输入",
           });
         });
-    }
+    },
   },
-  computed: {}
+  computed: {},
 };
 </script>
 
