cb0a41641839129120aad9c1af5f8ed87f6f6b42
tangzq <t1339803847@163.com>
2020-11-30 22:49:42 +0800

    完善bug页面

--- front/src/App.vue
+++ front/src/App.vue
@@ -7,27 +7,28 @@
  -->
 <template>
   <div id="app">
-      <div class="top" v-show=" $store.state.userInfo && $store.state.userInfo.id > 0 ">
+    <div
+      class="top"
+      v-show="$store.state.userInfo && $store.state.userInfo.id > 0"
+    >
       <topBar />
-     </div>
+    </div>
     <transition name="el-fade-in-linear" mode="out-in" appear>
       <div class="app-full">
-      <router-view></router-view>
+        <router-view></router-view>
       </div>
     </transition>
   </div>
 </template>
 
 <script>
-
 import TopBar from "./components/common/topBar";
 
 export default {
   name: "app",
-  created() {
-  },
-   components: {
-    topBar: TopBar
+  created() {},
+  components: {
+    topBar: TopBar,
   },
 };
 </script>
@@ -41,17 +42,15 @@ export default {
     "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
   width: 100%;
   height: 100%;
 }
 
-.top{
+.top {
   background-color: rgb(64, 158, 255);
 }
 .app-full {
-  width:85%;
-  margin:0 auto;
+  width: 85%;
+  margin: 0 auto;
 }
-
 </style>
--- front/src/components/bug/home.vue
+++ front/src/components/bug/home.vue
@@ -7,12 +7,16 @@
     </div>
     <div class="dialog">
       <el-dialog title="新建bug" :visible.sync="dialogFormVisible">
-        <el-form :model="form" ref="bugForm">
-          <el-form-item label="标题" :label-width="formLabelWidth">
+        <el-form :model="form" ref="bugForm" :rules="rules">
+          <el-form-item label="标题" :label-width="formLabelWidth" prop="title">
             <el-input v-model="form.title" autocomplete="off"></el-input>
           </el-form-item>
 
-          <el-form-item label="状态" :label-width="formLabelWidth">
+          <el-form-item
+            label="状态"
+            :label-width="formLabelWidth"
+            prop="status"
+          >
             <el-select v-model="form.status" placeholder="请选择状态">
               <el-option label="待处理" value="1"></el-option>
               <el-option label="已处理" value="2"></el-option>
@@ -20,7 +24,7 @@
             </el-select>
           </el-form-item>
 
-          <el-form-item label="等级" :label-width="formLabelWidth">
+          <el-form-item label="等级" :label-width="formLabelWidth" prop="level">
             <el-select v-model="form.level" placeholder="请选择等级">
               <el-option label="低" value="1"></el-option>
               <el-option label="中" value="2"></el-option>
@@ -28,7 +32,11 @@
             </el-select>
           </el-form-item>
 
-          <el-form-item label="描述" :label-width="formLabelWidth">
+          <el-form-item
+            label="描述"
+            :label-width="formLabelWidth"
+            prop="content"
+          >
             <el-input
               type="textarea"
               :rows="5"
@@ -37,7 +45,11 @@
             ></el-input>
           </el-form-item>
 
-          <el-form-item label="备注" :label-width="formLabelWidth">
+          <el-form-item
+            label="备注"
+            :label-width="formLabelWidth"
+            prop="comment"
+          >
             <el-input
               type="textarea"
               :rows="5"
@@ -46,7 +58,11 @@
             ></el-input>
           </el-form-item>
 
-          <el-form-item label="指派至:" :label-width="formLabelWidth">
+          <el-form-item
+            label="指派至:"
+            :label-width="formLabelWidth"
+            prop="to_user_id"
+          >
             <el-select v-model="form.to_user_id" placeholder="请选择用户">
               <el-option label="xiaobai" value="1"></el-option>
               <el-option label="xiaoxi" value="2"></el-option>
@@ -55,19 +71,20 @@
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
-          <el-button @click="resetForm('bugForm');dialogFormVisible = false;">取 消</el-button>
           <el-button
-            type="primary"
             @click="
+              resetForm('bugForm');
               dialogFormVisible = false;
-              createBug();
             "
+            >取 消</el-button
+          >
+          <el-button type="primary" @click="createBug('bugForm')"
             >确 定</el-button
           >
         </div>
       </el-dialog>
     </div>
-    <div class="content">
+    <div class="content"   v-loading="loading">
       <el-table
         :data="tableData.res"
         border
@@ -82,8 +99,31 @@
         <el-table-column prop="create_time" label="创建时间"> </el-table-column>
       </el-table>
     </div>
+    <div class="content-page">
+      <el-pagination
+        background
+        layout="total,prev, pager, next"
+        :total="parseInt(tableData.count)"
+        :page-size="ps"
+        :current-page="cp"
+        @current-change="changePage($event)"
+      >
+      </el-pagination>
+    </div>
     <div class="dialog-content">
-  
+      <el-dialog title="bug详情" :visible.sync="dialogContentFormVisible">
+        <ul>
+        <li><span>{{ currentRow.id }}</span></li>
+        <li><span>{{ currentRow.to_user_id }}</span></li>
+        <li><span>{{ currentRow.title }}</span></li>
+        <li><span>{{ currentRow.content }}</span></li>
+        <li><span>{{ currentRow.comment }}</span></li>
+        <li><span>{{ currentRow.status }}</span></li>
+        <li><span>{{ currentRow.level }}</span></li>
+        <li><span>{{ currentRow.create_time }}</span></li>
+        <li><span>{{ currentRow.update_time }}</span></li>
+          </ul>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -96,6 +136,9 @@ export default {
   },
   data() {
     return {
+      loading:false,
+      ps: 10,
+      cp: 1,
       tableData: {
         count: 0,
         res: [],
@@ -112,16 +155,28 @@ export default {
         status: null,
         level: null,
       },
-      detailForm:null,
+      detailForm: null,
       formLabelWidth: "140px",
+      currentRow: {},
+      rules: {
+        to_user_id: [
+          { required: true, message: "请选择指派", trigger: "blur" },
+        ],
+        title: [{ required: true, message: "请填入标题", trigger: "blur" }],
+        content: [{ required: true, message: "请填入描述", trigger: "blur" }],
+        comment: [],
+        status: [{ required: true, message: "请选择状态", trigger: "blur" }],
+        level: [{ required: true, message: "请选择等级", trigger: "blur" }],
+      },
     };
   },
   created() {
-    this.bugList(this.$route.params.id, 10, 1);
+    this.bugList(this.$route.params.id, this.ps, this.cp);
   },
 
   methods: {
     bugList(projectId, ps = 10, cp = 1) {
+      this.loading = true;
       this.$http
         .get("bug/list", {
           params: {
@@ -134,33 +189,46 @@ export default {
           let data = response.data.data;
           this.tableData.count = data.resCount;
           this.tableData.res = data.resItem;
+             this.loading = false;
         });
+     
     },
-    rowClick(row, column, event) {
+    rowClick(row) {
       this.dialogContentFormVisible = true;
-      console.log(row, column, event);
+      this.currentRow = row;
     },
-    createBug() {
-      this.$http
-        .post("bug/create", {
-          ...this.form,
-        })
-        .then((response) => {
-          let data = response.data;
-          if (data.code === 200) {
-            this.$message.success("保存成功");
-            this.resetForm("bugForm");
-            this.bugList(this.$route.params.id, 10, 1);
-          }else {
-             this.$message.error(data.msg);
-          }
-        });
+    createBug(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$http
+            .post("bug/create", {
+              ...this.form,
+            })
+            .then((response) => {
+              let data = response.data;
+              if (data.code === 200) {
+                this.$message.success("保存成功");
+                this.resetForm("bugForm");
+                this.bugList(this.$route.params.id, this.ps, this.cp);
+              } else {
+                this.$message.error(data.msg);
+              }
+            });
+          this.dialogFormVisible = false;
+        } else {
+          return false;
+        }
+      });
     },
-      resetForm(formName) {
-        this.$nextTick(() => {
+    resetForm(formName) {
+      this.$nextTick(() => {
         this.$refs[formName].resetFields();
-        });
-      }
+      });
+    },
+    changePage(event) {
+      this.cp = event;
+      this.bugList(this.$route.params.id, this.ps, this.cp);
+    },
   },
 };
 </script>
@@ -168,13 +236,11 @@ export default {
 <style lang="scss" scoped>
 .bug-home {
   .content {
-    .left {
-      border: 1px solid black;
-    }
+    min-height: 650px;
+  }
 
-    .right {
-      border: 1px solid black;
-    }
+  .content-page {
+    text-align: center;
   }
 
   .bar {
