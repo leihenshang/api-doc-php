9607759aa88a04ea56c90f36599c263ff139592a
csyuser <13540039093@163.com>
2020-12-17 15:18:15 +0800

    代理

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-
+  lintOnSave: false,
+  devServer: {
+    proxy:{
+      '/api': {
+        target: "www.321go.top:2001",
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/', // rewrite path
+        },
+      },
+    }
+  },
 }
