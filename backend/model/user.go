a24d8ca4167e685c6b4b8645b0ca9e73de7eada5
唐志强 <t1339803847@163.com>
2021-08-19 20:31:58 +0800

    更新模型

--- backend/model/user.go
+++ backend/model/user.go
@@ -5,25 +5,27 @@ import (
 )
 
 // User 用户表
+
 type User struct {
-	BasicModel
-	NickName      string    `gorm:"column:nick_name;type:varchar(100)" json:"nickName"`                                 // 展示昵称可以更改
-	Account       string    `gorm:"uniqueIndex:user_info_UN1;column:account;type:varchar(100);not null" json:"account"` // 登录账号名不可更改
-	Email         string    `gorm:"uniqueIndex:user_info_UN1;column:email;type:varchar(100)" json:"email"`              // 邮箱
-	Password      string    `gorm:"column:password;type:varchar(100);not null" json:"password"`                         // 密码
-	UserType      uint8     `gorm:"column:user_type;type:tinyint(3) unsigned;not null;default:3" json:"userType"`       // 用户类型1超级管理员2管理员3用户
-	Status        uint8     `gorm:"column:status;type:tinyint(3) unsigned;not null;default:1" json:"status"`            // 1正常2禁用
-	IsActivated   uint8     `gorm:"column:is_activated;type:tinyint(3) unsigned;not null;default:1" json:"isActivated"` // 是否激活,1-已激活,2-未激活
-	Mobile        string    `gorm:"column:mobile;type:char(11)" json:"mobile"`                                          // 手机号
-	Avatar        string    `gorm:"column:avatar;type:varchar(100)" json:"avatar"`                                      // 头像
-	Description   string    `gorm:"column:description;type:varchar(200);not null;default:无" json:"description"`         // 自我说明
-	Token         string    `gorm:"column:token;type:varchar(100)" json:"token"`                                        // 访问token
-	TokenExpire   time.Time `gorm:"column:token_expire;type:datetime" json:"tokenExpire"`                               // token过期时间
-	LastLoginIP   string    `gorm:"column:last_login_ip;type:varchar(100)" json:"lastLoginIp"`                          // 最后登陆ip
-	LastLoginTime time.Time `gorm:"column:last_login_time;type:datetime" json:"lastLoginTime"`
+	Id            uint64    `gorm:"column:id;type:bigint(20) unsigned;primary_key;AUTO_INCREMENT"`
+	Nickname      string    `gorm:"column:nickname;type:varchar(50);NOT NULL"`                    // 昵称
+	Account       string    `gorm:"column:account;type:varchar(100);NOT NULL"`                    // 账号
+	Email         string    `gorm:"column:email;type:varchar(100)"`                               // 邮箱
+	Password      string    `gorm:"column:password;type:varchar(100);NOT NULL"`                   // 密码
+	UserType      uint      `gorm:"column:user_type;type:tinyint(3) unsigned;default:1;NOT NULL"` // 1-普通用户,2管理员,100超级管理员
+	Status        uint      `gorm:"column:status;type:tinyint(3) unsigned;default:1;NOT NULL"`    // 1-可用,2-不可用,3-未激活
+	Mobile        string    `gorm:"column:mobile;type:char(11)"`                                  // 手机号
+	IconPath      string    `gorm:"column:icon_path;type:varchar(100)"`                           // 头像地址
+	Bio           string    `gorm:"column:bio;type:varchar(200)"`                                 // 个人说明
+	Token         string    `gorm:"column:token;type:varchar(100)"`                               // 登陆token
+	TokenExpire   time.Time `gorm:"column:token_expire;type:datetime"`                            // token超时时间
+	LastLoginIp   string    `gorm:"column:last_login_ip;type:varchar(100)"`                       // 最后登陆ip地址
+	LastLoginTime time.Time `gorm:"column:last_login_time;type:datetime"`                         // 最后登陆时间
+	DeletedAt     time.Time `gorm:"column:deleted_at;type:datetime(3)"`                           // 删除时间
+	CreatedAt     time.Time `gorm:"column:created_at;type:datetime(3)"`                           // 创建时间
+	UpdatedAt     time.Time `gorm:"column:updated_at;type:datetime(3)"`                           // 更新时间
 }
 
-// TableName get sql table name.获取数据库表名
 func (m *User) TableName() string {
 	return "user"
 }
