16ea66d4685bf263c3dfeb6400d0db99d6805997
tangq <t1339803847@163.com>
2021-08-22 13:11:28 +0800

    更新配置模板文件

--- backend/config/db copy.php
+++ backend/config/db copy.php
@@ -5,7 +5,7 @@ return [
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
-    'charset' => 'utf8',
+    'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
