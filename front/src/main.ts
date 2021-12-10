03c8a6ad8c898e7945a648ca8effebf762f360bc
csyuser <13540039093@163.com>
2021-12-10 15:56:56 +0800

    引入layui

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import Layui from '@layui/layui-vue'
+import '@layui/layui-vue/lib/index.css'
 import {router} from './router';
 
 let app = createApp(App)
 app.use(router)
+app.use(Layui)
 app.mount('#app')
