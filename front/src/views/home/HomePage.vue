99ae0aada116cdfa3fb51533f268eb198f93d83f
csyuser <13540039093@163.com>
2022-01-07 16:46:38 +0800

    自定义更改icon未成功

--- front/src/views/home/HomePage.vue
+++ front/src/views/home/HomePage.vue
@@ -6,7 +6,7 @@
                       :collapsed="collapsed" @collapse="collapsed = true" @expand="collapsed = false">
         <n-menu class="menu-menu" :collapsed="collapsed" :collapsed-width="64" :collapsed-icon-size="22"
                 :options="menuOptions" :indent="24" :render-label="renderMenuLabel" :default-value="route.path"
-                :render-icon="renderMenuIcon" :expand-icon="expandIcon"/>
+                :render-icon="renderMenuIcon"/>
       </n-layout-sider>
       <n-layout>
         <router-view></router-view>
@@ -20,26 +20,31 @@ import Header from '../../components/Header.vue';
 import {h, ref,onMounted} from 'vue';
 import { RouterLink,useRoute} from 'vue-router'
 import {NIcon} from 'naive-ui';
-import {BookmarkOutline, CaretDownOutline} from '@vicons/ionicons5';
+import {BookmarkOutline} from '@vicons/ionicons5';
 
 const menuOptions = [
   {
     label: '项目列表',
     pathName:'ProjectList',
     key: '/ProjectList',
+    iconName:BookmarkOutline,
   }, {
     label: '用户管理',
     pathName:'UserManagement',
     key: '/UserManagement',
+    iconName:BookmarkOutline,
   }, {
     label: '小记',
     key: 'node',
+    iconName:BookmarkOutline,
   }, {
     label: '最近浏览',
     key: 'RecentBrowse',
+    iconName:BookmarkOutline,
   }, {
     label: '收藏',
     key: 'like',
+    iconName:BookmarkOutline,
   }
 ];
 
@@ -66,14 +71,12 @@ export default {
       },
       renderMenuIcon(option) {
         // 渲染图标占位符以保持缩进
-        if (option.key === 'sheep-man') return true;
+        // if (option.key === 'sheep-man') return true;
         // 返回 falsy 值，不再渲染图标及占位符
-        if (option.key === 'food') return null;
-        return h(NIcon, null, {default: () => h(BookmarkOutline)});
+        // if (option.key === 'food') return null;
+        // return h(NIcon, null, {default: () => h(BookmarkOutline)});
+        return option.iconName && h(NIcon, null, {default: () => h(option.iconName)});
       },
-      expandIcon() {
-        return h(NIcon, null, {default: () => h(CaretDownOutline)});
-      }
     };
   }
 };
